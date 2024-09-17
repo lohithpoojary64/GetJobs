ffb6add361e11b558cb21b29b0ecc0fafd16a0c5
Lohith <lohithpoojary64@gmail.com>
2024-09-17 16:00:49 +0530

    Initial commit

---
+++ src/app/home/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Navbar from '../../components/Home/Navbar';
+
+const page = () => {
+  return (
+    <div className='min-h-screen'>
+      <Navbar/>
+    </div>
+  )
+}
+
+export default page
---
+++ src/components/Home/Navbar.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='h-[100px] w-[100%]  px-10 flex justify-between items-center'>
+      <h1 className='text-green-500 font-extrabold text-[20px]'>GET-JOBS</h1>
+      <div className="">
+        <li>Communities</li>
+        <li>Jobs</li>
+        <li>Companies</li>
+        <li>Communities</li>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
