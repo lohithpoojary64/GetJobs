a1788aacd90db9a0f565657862c4fe827d54344b
Lohith <lohithpoojary64@gmail.com>
2024-10-01 13:11:01 +0530

    changed folder structure

---
+++ frontend/src/app/sign-up/page.tsx
@@ -0,0 +1,13 @@
+import SignUp from "../../components/signUp/Signup";
+import React from "react";
+
+
+const page = () => {
+  return (
+    <div className="h-screen w-full flex justify-center items-center">
+     <SignUp/>
+    </div>
+  );
+};
+
+export default page;
---
+++ frontend/src/components/signUp/SignUp.tsx
@@ -0,0 +1,56 @@
+'use client'
+import React from "react";
+import { Button, Checkbox, Form, Input } from "antd";
+
+interface FieldType{
+    email:string;
+    password:string;
+    remember:string;
+}
+
+const Signup = () => {
+  return (
+    <div>
+      <Form
+        name="basic"
+        labelCol={{ span: 8 }}
+        wrapperCol={{ span: 16 }}
+        style={{ maxWidth: 600 }}
+        initialValues={{ remember: true }}
+        autoComplete="off"
+      >
+        <Form.Item<FieldType>
+          label="Email"
+          name="email"
+          rules={[{ required: true, message: "Please input your Email!" }]}
+        >
+          <Input />
+        </Form.Item>
+
+        <Form.Item<FieldType>
+          label="Password"
+          name="password"
+          rules={[{ required: true, message: "Please input your password!" }]}
+        >
+          <Input.Password />
+        </Form.Item>
+
+        <Form.Item<FieldType>
+          name="remember"
+          valuePropName="checked"
+          wrapperCol={{ offset: 8, span: 16 }}
+        >
+          <Checkbox>Remember me</Checkbox>
+        </Form.Item>
+
+        <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+          <Button type="primary" htmlType="submit">
+            Submit
+          </Button>
+        </Form.Item>
+      </Form>
+    </div>
+  );
+};
+
+export default Signup;
